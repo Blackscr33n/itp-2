f7ea6a038acb8d1d1a74790a8140c9b613d67ffa
Miehl <bmiehl@deloitte.at>
2018-08-14 10:30:26 +0200

    rm build-folder

--- src/config/mongodb-setup.js
+++ src/config/mongodb-setup.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
-var url = "mongodb://localhost:27017/mydb"
+var url = `mongodb://${process.env.MDB_USER}:${process.env.MDB_PASS}@ds219832.mlab.com:19832/heroku_90t1mdnn`
 
 const db
 
