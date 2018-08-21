86aebfb386fd7e106139bdd602b49c2a4331c527
Miehl <bmiehl@deloitte.at>
2018-08-21 09:42:35 +0200

    rm backpack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ var express = require('express')
 require('dotenv').load()
 var app = express()
 
+app.use(express.static('assets'))
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
@@ -12,8 +13,8 @@ app.set('view engine', 'ejs')
 // index page 
 app.get('/', function(req, res) {
     res.render('pages/index', {
-        'title' : 'awesome',
-        'content': 'Test  das ist ein cooler Paragraph - asd  asd dasda &uuml;',
+        'title' : 'Awesome!',
+        'content': 'Lorem Ipsum',
         'menuEntries': [
             { name: 'Home', url: '/' },
             { name: 'About', url: '/about' }
